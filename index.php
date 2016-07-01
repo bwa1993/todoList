a92087ac50f4d5da7ea87cd808217679d3bd919e
Brandon Alexander <MeThudZ@Brandons-MacBook-Air.local>
2016-06-30 22:27:53 -0700

    First commit

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php
+
+require_once 'init.php';
+
+$itemsQuery = $db->prepare("
+	SELECT id, name, done
+	FROM items
+	WHERE user = :user
+");
+
+$itemsQuery->execute([
+	'user' => $_SESSION['user_id']
+]);
+
+$items = $itemsQuery->rowCount() ? $itemsQuery : [];
+
+print_r($items);
+
+//foreach($items as $item) {
+//	echo $item['name'], '<br>';
+//}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>To Do List</title>
+
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Shadows+Into+Light+Two" rel="stylesheet">
+		<link rel="stylesheet" href="main.css">
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	</head>
+	<body>
+		<div class="list">
+			<h1 class="header">To do.</h1>
+
+			<?php if(!empty($items)): ?>
+			<ul class="items">
+				<?php foreach($items as $item): ?>
+					<li>
+						<span class="item"<?php echo $item['done'] ? ' done' : ' ' ?><?php echo $item['name']; ?></span>
+						<a href="#" class="done-button">Mark as done</a>
+					</li>
+				<?php endforeach; ?>
+			</ul>
+			<?php else: ?>
+				<p>You haven't added any items yet.</p>
+			<?php endif; ?>
+
+			<form class="item-add" action="add.php" method="post">
+				<input type="text" name="name" placeholder="Type a new item here." class="input" autocomplete="off" required>
+				<input type="submit" value="Add" class="submit">
+			</form>
+
+		</div>
+	</body>
+</html>
